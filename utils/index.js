f3d62c42510f650411e623e790c477b8ef429152
dinushchathurya <dinushchathurya21@gmail.com>
2022-05-15 14:09:29 +0530

    :ambulance: Fix issue in phone number validate function

--- utils/index.js
+++ utils/index.js
@@ -2,52 +2,56 @@ const { phone } = require("../data/phone");
 
 const standardizeMSISDN = (number) => {
 
-  const phoneLength = `${number}`.length;
+    const phoneLength = `${number}`.length;
+  
+    const isValid = phone.regex(number);
+  
+    if (!isValid) return "";
 
-  const isValid = phone.regex(number);
-
-  if (!isValid) return "";
-
-  switch (phoneLength) {
-    case 9:
-      return number;
-    case 10:
-      return `${number}`.substring(1);
-    case 12:
-      return `${number}`.substring(3);
-    case 13:
-      return `${number}`.substring(4);
-    default:
-      return "";
-  }
-
+    switch (phoneLength) {
+        case 9:
+          return number;
+        case 10:
+          return `${number}`.substring(1);
+        case 12:
+          return `${number}`.substring(3);
+        case 13:
+          return `${number}`.substring(4);
+        default:
+          return "";
+    }
 };
 
 exports.validatePhone = (phone) => {
 
-  const msisdn = standardizeMSISDN(phone);
+    const msisdn = standardizeMSISDN(phone);
 
-  let isValid = false;
+    let isValid = false;
 
-  const resp = (status) => {
-    return {
-        phone: msisdn ? msisdn : phone,
-        valid: status
+    const resp = (status, info) => {
+        return {
+            phone: msisdn ? msisdn : phone,
+            valid: status,
+            info,
+        };
     };
-  };
 
-  if (msisdn != "") {
+    if (msisdn != "") {
 
-    const info = isValid
-      ? "Is valid srilankan phone number"
-      : `${msisdn} is not a valid ${provider.name} phone number`;
+      isValid = true;
 
-    return resp(isValid, info, telco ? telco : "unknown");
-  } else {
-    return resp(
-      false,
-      `Phone number ${phone} is not a valid srilankan number`,
-      "unknown"
-    );
-  }
+      const info = isValid? "Is valid srilankan phone number": `${msisdn} is not a valid srilankan phone number`;
+  
+      return resp(
+          isValid, 
+          info
+      );
+
+    } else {
+      return resp(
+          false,
+          `Phone number ${phone} is not a valid srilankan number`,
+          "unknown"
+      );
+    }
 };
